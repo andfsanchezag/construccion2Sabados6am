83c4b6f1805abc2f2e901626fcfc6a782fb25e33
andfsanchezag <andres.sanchez57@tdea.edu.co>
2024-08-05 09:28:08 -0500

    CHG: modify controllers

--- proyectoEjemplo/src/main/java/app/App.java
+++ proyectoEjemplo/src/main/java/app/App.java
@@ -4,11 +4,11 @@ import app.controller.LoginController;
 
 public class App {
 
-	public static void main(String[] args) throws Exception{
+	public static void main(String[] args) throws Exception {
 		LoginController veterinarianController = new LoginController();
 		try {
-		veterinarianController.session();}
-		catch(Exception e) {
+			veterinarianController.session();
+		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
 	}
